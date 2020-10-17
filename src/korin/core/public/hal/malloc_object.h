8b65f8ceff4ab07cf22c82a17905f6f077e00df4
sneppy <andrea.mecchia@mail.polimi.it>
2020-10-17 23:24:55 +0200

    Refactor of MallocObject class (breaks Array class, but I was gonna refactor it anyway) + Specialization for void allocator type, only handles non-managed allocators of any type; + Generalization for allocator type, has managed allocator as class member; - Unique class for both removed.

--- src/korin/core/public/hal/malloc_object.h
+++ src/korin/core/public/hal/malloc_object.h
@@ -1,42 +1,49 @@
 #pragma once
 
 #include "core_types.h"
-#include "platform_memory.h"
-#include "../templates/utility.h"
+#include "misc/assert.h"
+#include "templates/utility.h"
+#include "./platform_memory.h"
+
+template<typename, typename = void> class MallocObject;
 
 /**
+ * Wrapper around a malloc class that
+ * handles allocation for a specific
+ * type of object T.
  * 
+ * When provided, the second template
+ * parameter specify an allocator class
+ * to spawn, if none specified.
+ * 
+ * @param T type of the object
+ * @param MallocT allocator type, or void
  */
-template<typename T, typename MallocT>
-class MallocObject
+template<typename T>
+class MallocObject<T, void> : public NonCopyable
 {
-protected:
-	/// Allocation unit size
-	constexpr static sizet allocUnitSize = sizeof(T);
-
-	/// Allocation alignment
-	constexpr static sizet allocUnitAlign = alignof(T);
-
-	/// Pointer to the used malloc
-	MallocT * malloc;
-
-	/// Has own allocator flag
-	bool bHasOwnMalloc;
-
 public:
 	/**
-	 * Create object allocator with given malloc.
+	 * Remove default constructor
 	 */
-	explicit FORCE_INLINE MallocObject(MallocT * inMalloc = nullptr)
+	MallocObject() = delete;
+
+	/**
+	 * Creates object allocator with given malloc.
+	 * 
+	 * @param inMalloc allocator that will
+	 * 	be used to allocate objects
+	 */
+	explicit FORCE_INLINE MallocObject(MallocBase * inMalloc)
 		: malloc{inMalloc}
 		, bHasOwnMalloc{inMalloc == nullptr}
 	{
-		// Create default if none specified
-		if (bHasOwnMalloc) malloc = new MallocT;
+		CHECKF(!!inMalloc, "Provided allocator cannot be NULL")
 	}
 
 	/**
-	 * Move constructor.
+	 * Move constructor. Move allocator and reset
+	 * own allocator flag.
 	 */
 	FORCE_INLINE MallocObject(MallocObject && other)
 		: malloc{other.malloc}
@@ -47,12 +54,15 @@ public:
 	}
 
 	/**
-	 * Move operator.
+	 * Move operator. If has own allocator,
+	 * delete it and move other allocator
+	 * instead.
 	 */
 	FORCE_INLINE MallocObject & operator=(MallocObject && other)
 	{
-		// Delete and replace managed allocator
+		// If has managed allocator, delete it
 		if (bHasOwnMalloc) delete malloc;
+
 		malloc = other.malloc;
 		bHasOwnMalloc = other.bHasOwnMalloc;
 
@@ -61,21 +71,10 @@ public:
 
 		return *this;
 	}
-	
-	/**
-	 * List constructor, arguments are passed
-	 * to underlying allocator constructor.
-	 */
-	template<typename ... Args>
-	FORCE_INLINE MallocObject(Args && ... args)
-		: bHasOwnMalloc{true}
-	{
-		// Create allocator
-		malloc = new MallocT{forward<Args>(args) ...};
-	}
 
 	/**
-	 * Destructor.
+	 * Destructor, deallocates managed allocator
+	 * and sets pointer to NULL.
 	 */
 	FORCE_INLINE ~MallocObject()
 	{
@@ -83,17 +82,17 @@ public:
 		{
 			// Delete managed allocator
 			delete malloc;
-
 			bHasOwnMalloc = false;
-			malloc = nullptr;
 		}
+
+		malloc = nullptr;
 	}
 	
 	/**
 	 * Alloc space for n objects of type T.
 	 * 
 	 * @param n num of objects
-	 * @returns address of alloated memory
+	 * @returns pointer to allocated memory
 	 */
 	FORCE_INLINE T * alloc(uint64 n)
 	{
@@ -105,7 +104,7 @@ public:
 	 * 
 	 * @param orig previously allocated memory
 	 * @param n num of objects
-	 * @returns address of alloated memory
+	 * @returns pointer to alloated memory
 	 */
 	FORCE_INLINE T * realloc(T * orig, uint64 n)
 	{
@@ -121,10 +120,42 @@ public:
 	{
 		malloc->free((void*)orig);
 	}
+
+protected:
+	/// Allocation unit size
+	static constexpr const sizet allocUnitSize = sizeof(T);
+
+	/// Allocation alignment
+	static constexpr const sizet allocUnitAlign = alignof(T);
+
+	/// Pointer to the used allocator
+	MallocBase * malloc;
+
+	/// Has own allocator, flag
+	ubyte bHasOwnMalloc : 1;
 };
 
+/**
+ * Generalization for type of allocator.
+ * @see MallocObject
+ */
 template<typename T, typename MallocT>
-constexpr sizet MallocObject<T, MallocT>::allocUnitSize;
+struct MallocObject : public MallocObject<T, void>
+{
+	using Base = MallocObject<T, void>;
 
-template<typename T, typename MallocT>
-constexpr sizet MallocObject<T, MallocT>::allocUnitAlign;
+	/**
+	 * 
+	 */
+	template<typename ...MallocCreateArgsT>
+	FORCE_INLINE explicit MallocObject(MallocCreateArgsT && ...createArgs)
+		: Base{&managedMalloc}
+		, managedMalloc{forward<MallocCreateArgsT>(createArgs)...}
+	{
+		//
+	}
+
+protected:
+	/// Stack allocated allocator
+	MallocT managedMalloc;
+};
